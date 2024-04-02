501e55bd34b862825095afa23f1b8ab56174f641
Afua <etutuwaa.88@gmail.com>
2024-04-02 22:20:51 +0300

    Includes an API for currency conversion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # insurance-cost-calculator
+
 def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
     estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
     print(f"The estimated insurance cost for {name} is {estimated_cost} dollars")
@@ -15,10 +16,19 @@ def get_user_input():
     return name, age, sex, bmi, num_of_children, smoker
 
 
+def convert_currency(amount, from_currency, to_currency, requests=None):
+    url = f"https://api.exchangerate-api.com/v4/latest/{from_currency}"
+    response = requests.get(url)
+    data = response.json()
+    rate = float(data[to_currency])
+    return amount * rate
+
+
 def main():
     print("Welcome to the Insurance Cost Calculator!")
     name, age, sex, bmi, num_of_children, smoker = get_user_input()
     calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker)
+
 
 
 if __name__ == "__main__":
